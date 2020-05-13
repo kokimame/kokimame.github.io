f42f7acfaa4a7019fa05d1574b9fb285b2399c17
kokimame <code.kohkimametani@gmail.com>
2020-05-13 22:08:24 +0200

    Update education

--- js/scripts.js
+++ js/scripts.js
@@ -63,7 +63,7 @@
 
         // Add icons to each block
         $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-graduation-cap"></i></div>');
         });
 
         // Add dates to the timeline if exists
